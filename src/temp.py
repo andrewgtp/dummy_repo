2a18d35666880aba3c70f8ab0955663b123053fc
Andrew <andrewgtp@gmail.com>
2014-01-12 17:15:42 -0800

    Added copyright and python33 compatible

--- src/temp.py
+++ src/temp.py
@@ -6,9 +6,12 @@ Created on Jan 10, 2014
 # encoding: utf-16
 def print_out(greetings):
     for greeting in greetings:
-        print greeting
+        print (greeting)
+    
+    print(copyright)
     
 if __name__ == "__main__":
     
     greetings = [ 'HELLLLO', 'Bonjiorno', 'Buenos Dias' ]
+    copyright = 'Copyright \xa9 iBitchin inc., All rights reserved'
     print_out(greetings)    
